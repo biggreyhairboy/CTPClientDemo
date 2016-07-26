59170f9ae0cfeb9b558872f3326732c5fb7685a3
biggreyhairboy <biggreyhairboy@gmail.com>
2016-07-26 13:15:16 +0800

    first time can be compiled and connect to simnow successfully but something wrong with the content

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 CThostFtdcMdApi *pUserApi;
 
-char FRONT_ADDR_quote[] = "tcp://180.168.146.187.10031";
-char FRONT_ADDR_trade[] = "tcp://180.168.146.187.10030";
-TThostFtdcBrokerIDType BROKER_ID = "9999";
-TThostFtdcInvestorIDType INVESTOR_ID = "039395";
-TThostFtdcPasswordType PASSWORD = "yjk19890412";
-char *ppInstrumentID[] = {"rb1610", "rb1701"};
+char FRONT_ADDR[] = "tcp://180.168.146.187:10001";
+char FRONT_ADDR_trade[] = "tcp://180.168.146.187:10011";
+TThostFtdcBrokerIDType brokerIDType = "9999";
+TThostFtdcInvestorIDType investorIDType = "039395";
+TThostFtdcPasswordType passwordType = "yjk19890412";
+char *ppIntrumentID[] = {"rb1610", "rb1701"};
 int iInstrumentID = 2;
 
 void quoteThread()
@@ -20,7 +20,7 @@ void quoteThread()
     pUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
     CThostFtdcMdSpi *pMarketDataHandle = new MarketDataHandle();
     pUserApi->RegisterSpi(pMarketDataHandle);
-    pUserApi->RegisterFront(FRONT_ADDR_quote);
+    pUserApi->RegisterFront(FRONT_ADDR);
     pUserApi->Init();
     pUserApi->Join();
 }
